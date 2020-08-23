7e1bc08e6b7ab924a923f27251b8b02b936e0e8b
Abel de la Fuente <65258220+abelfubu@users.noreply.github.com>
2020-08-23 11:36:52 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 
 import express from 'express';
-import path from 'path';
+import cors from 'cors';
+import morgan from 'morgan';
 
 import indexRouter from './routes/index';
 
 const app = express();
 
 app.use(express.static('dist/public'));
-
+app.use(cors());
+app.use(morgan('tiny'));
 app.use('/', indexRouter);
 
 app.listen(3000, () => console.log('PORT 3000 ON FIRE!'));

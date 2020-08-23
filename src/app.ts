c8604544fadbb33963c5fd9c9205c797d4031d52
Abel de la Fuente <65258220+abelfubu@users.noreply.github.com>
2020-08-23 10:38:43 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,8 @@ import indexRouter from './routes/index';
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
-
 app.use(express.static('dist/public'));
 
 app.use('/', indexRouter);
-
-console.log(process.env.DB_USER)
 
 app.listen(3000, () => console.log('PORT 3000 ON FIRE!'));

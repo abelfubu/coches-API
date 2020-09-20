e9b50de885fce3ffed791a850156d594ecff7d76
abelfubu <abelfubu@gmail.com>
2020-09-20 15:25:56 +0200

    add passport

--- src/middleware/check-auth.ts
+++ src/middleware/check-auth.ts
@@ -1,16 +1,19 @@
-import { RequestHandler } from 'express';
-import jwt from 'jsonwebtoken';
+import { Strategy, ExtractJwt, StrategyOptions } from 'passport-jwt';
 
-export const auth: RequestHandler = (req, res, next) => {
-  try {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) {
-      return res.status(401).json({ message: 'Unauthorized' });
-    }
-    console.log(jwt.verify(token, process.env.SECRET as string));
-    req.body.userData = jwt.verify(token, process.env.SECRET as string);
-    next();
-  } catch (error) {
-    res.status(401).json({ message: 'Auth failed!' });
-  }
+const options: StrategyOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.SECRET,
 };
+
+export const auth = new Strategy(options, async (payload, done) => {
+  try {
+    const user = { id: 456 };
+    // const user = await () => { } //find user in db by payload.id
+    if (user) {
+      return done(null, user);
+    }
+    return done(null, false);
+  } catch (error) {
+    console.log(error);
+  }
+});
--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import * as indexController from '../controllers/index-controller';
-
-const router = Router();
-
-router.get('/', indexController.getAll);
-
-export default router;

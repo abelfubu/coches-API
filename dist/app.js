b188fc1e3ff70f06e0b8ad0ecba8dec22ba0cfb6
abelfubu <abelfubu@gmail.com>
2020-11-18 08:57:52 +0100

    API transalted to english

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,20 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const dotenv_1 = __importDefault(require('dotenv'));
 dotenv_1.default.config();
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const morgan_1 = __importDefault(require("morgan"));
-const passport_1 = __importDefault(require("passport"));
-const check_auth_1 = require("./middleware/check-auth");
-const viajes_routes_1 = __importDefault(require("./routes/viajes.routes"));
-const coches_routes_1 = __importDefault(require("./routes/coches.routes"));
-const auth_routes_1 = __importDefault(require("./routes/auth.routes"));
+const express_1 = __importDefault(require('express'));
+const cors_1 = __importDefault(require('cors'));
+const morgan_1 = __importDefault(require('morgan'));
+const passport_1 = __importDefault(require('passport'));
+const check_auth_1 = require('./middleware/check-auth');
+const viajes_routes_1 = __importDefault(require('./routes/viajes.routes'));
+const coches_routes_1 = __importDefault(require('./routes/coches.routes'));
+const auth_routes_1 = __importDefault(require('./routes/auth.routes'));
 const app = express_1.default();
 app.use(express_1.default.static('dist/public'));
 app.use(cors_1.default());
@@ -21,8 +23,8 @@ app.use(express_1.default.json());
 app.use(passport_1.default.initialize());
 passport_1.default.use(check_auth_1.auth);
 // app.use('/', indexRouter);
-app.use('/coches', coches_routes_1.default);
-app.use('/viajes', viajes_routes_1.default);
+app.use('/cars', coches_routes_1.default);
+app.use('/travels', viajes_routes_1.default);
 app.use('/auth', auth_routes_1.default);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`PORT ${PORT} ON FIRE!`));
--- dist/routes/viajes.routes.js
+++ dist/routes/viajes.routes.js
@@ -1,36 +1,60 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
+'use strict';
+var __createBinding =
+  (this && this.__createBinding) ||
+  (Object.create
+    ? function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        Object.defineProperty(o, k2, {
+          enumerable: true,
+          get: function () {
+            return m[k];
+          },
+        });
+      }
+    : function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        o[k2] = m[k];
+      });
+var __setModuleDefault =
+  (this && this.__setModuleDefault) ||
+  (Object.create
+    ? function (o, v) {
+        Object.defineProperty(o, 'default', { enumerable: true, value: v });
+      }
+    : function (o, v) {
+        o['default'] = v;
+      });
+var __importStar =
+  (this && this.__importStar) ||
+  function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    if (mod != null)
+      for (var k in mod)
+        if (k !== 'default' && Object.prototype.hasOwnProperty.call(mod, k))
+          __createBinding(result, mod, k);
     __setModuleDefault(result, mod);
     return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const passport_1 = __importDefault(require("passport"));
-const viajesController = __importStar(require("../controllers/viajes-controller"));
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = require('express');
+const passport_1 = __importDefault(require('passport'));
+const viajesController = __importStar(require('../controllers/viajes-controller'));
 const router = express_1.Router();
 router
-    .get('/', viajesController.getAll)
-    .post('/', passport_1.default.authenticate('jwt', { session: false }), viajesController.addOne)
-    .get('/special', passport_1.default.authenticate('jwt', { session: false }), viajesController.getSpecial)
-    .get('/:id', passport_1.default.authenticate('jwt', { session: false }), viajesController.getOne)
-    .put('/:id', passport_1.default.authenticate('jwt', { session: false }), viajesController.updateOne)
-    .delete('/:id', passport_1.default.authenticate('jwt', { session: false }), viajesController.deleteOne);
+  .get('/', viajesController.getAll)
+  .post('/', viajesController.addOne)
+  .get(
+    '/special',
+    passport_1.default.authenticate('jwt', { session: false }),
+    viajesController.getSpecial
+  )
+  .get('/:id', viajesController.getOne)
+  .put('/:id', viajesController.updateOne)
+  .delete('/:id', viajesController.deleteOne);
 exports.default = router;

0d36082303ad8f884d8bb24256dc02346307d112
abelfubu <abelfubu@gmail.com>
2020-12-02 13:11:04 +0100

    fix updating car

--- dist/controllers/coches-controller.js
+++ dist/controllers/coches-controller.js
@@ -34,8 +34,13 @@ exports.updateOne = (req, res) => {
     if (req.params.id) {
       const { id } = req.params;
       const newCoche = { ...req.body };
-      const pos = coches_1.coches.findIndex(coche => coche.id === id);
-      coches_1.coches.splice(pos, 1, req.body);
+      coches_1.coches.forEach(coche => {
+        if ((coche.id = id)) {
+          coche = { ...coche, ...req.body };
+        }
+      });
+      // const pos = coches_1.coches.findIndex(coche => coche.id === id);
+      // coches_1.coches.splice(pos, 1, req.body);
       res.status(200).json(newCoche);
     }
   } catch (error) {
